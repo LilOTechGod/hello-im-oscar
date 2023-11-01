8d9e8f8526fee201bf0b7400314a01836b4943f8
Oscar Perez-Hernandez <perezoscar360@gmail.com>
2023-10-31 22:46:21 -0500

    I created a type writing effect for my portfolio. Added all code to this to the App.js file inside SRC folder.

--- src/App.js
+++ src/App.js
@@ -2,12 +2,30 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/Header';
 import Hero from './components/Hero';
+import React from 'react';
+import Typewriter from 'typewriter-effect';
 
 function App() {
   return (
-    <div className="App">
+    <div className="">
       <Header />
       <Hero />
+      <div className="App">
+       <Typewriter onInit={(typewriter) => {
+            typewriter
+                .pauseFor(2000)
+                .typeString("I'm a Full Stack Software Engineer")
+                .pauseFor(2000)
+                .deleteChars(17)
+                .typeString("web developer")
+                .pauseFor(2000)
+                .deleteChars(24)
+                .typeString("problem solver")
+                .pauseFor(2000)
+                .start();
+        }}
+    />
+    </div>
     </div>
   );
 }
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,62 +2,8 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import video from '../assets/video.webm';
 import './Hero.css';
-import React, { useState, useEffect } from "react";
 
-function Hero() {
-
-// A custom hook that takes a text array and a delay as arguments
-// and returns the current text to display
-const useAutoTyping = (texts, delay) => {
-  // The index of the current text in the array
-  const [index, setIndex] = useState(0);
-  // The current text to display
-  const [text, setText] = useState("");
-  // The current character position in the text
-  const [position, setPosition] = useState(0);
-
-  // A useEffect hook that runs every time the index or position changes
-  useEffect(() => {
-    // If the position is equal to the length of the current text, move to the next text
-    if (position === texts[index].length) {
-      // Set a timeout to wait before changing the text
-      const timer = setTimeout(() => {
-        // Increment the index by one, or reset it to zero if it reaches the end of the array
-        setIndex((index + 1) % texts.length);
-        // Reset the position to zero
-        setPosition(0);
-      }, delay);
-
-      // Clear the timeout when the component unmounts or the index changes
-      return () => clearTimeout(timer);
-    }
-
-    // Set a timeout to update the text by adding one character
-    const timer = setTimeout(() => {
-      setText(texts[index].slice(0, position + 1));
-      // Increment the position by one
-      setPosition(position + 1);
-    }, delay);
-
-    // Clear the timeout when the component unmounts or the position changes
-    return () => clearTimeout(timer);
-  }, [index, position, texts, delay]);
-
-  // Return the current text
-  return text;
-};
-
-// A component that uses the custom hook and renders the text in a h1 element
-const AutoTypingText = () => {
-  // An array of texts to display
-  const texts = ["Hello", "World", "This is React"];
-  // A delay in milliseconds between each character
-  const delay = 100;
-  // The current text from the custom hook
-  const text = useAutoTyping(texts, delay);
-
-  return <h1>{text}</h1>;
-};
+export default function Hero() {
 
     return (
       <div>
@@ -71,11 +17,9 @@ const AutoTypingText = () => {
             sm=4
           </Col>
         </Row>
-        <div className='container heroDivider'></div>
+        <div className='container heroDivider'>
 
+        </div>
       </div>
     );
   }
-  
-  export AutoTypingText;
-  export default Hero;
