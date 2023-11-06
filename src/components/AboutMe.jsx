132445e8f99554d322aee47b5793bff89db59205
Oscar Perez-Hernandez <perezoscar360@gmail.com>
2023-11-06 12:24:13 -0600

    added my about me paragraph. It's a rough draft for now.

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -23,7 +23,7 @@ function AboutMe() {
             </Card.Body>
             </Card>
         </Col>
-        <Col sm={6} className='bg-danger'>sm=4</Col>
+        <Col sm={6} className='bg-danger'>Passionate and solutions-driven full stack web developer. To provide unique perspectives on how end-users interact with websites and software platforms is the goal. Innovative problem-solver who is passionate about developing apps with a focus on mobile-first design and development. Strengths in creativity, teamwork, and building projects from ideation to execution. Experience in utilizing a combination of strong communication, collaboration, and analytical skills. Strong ability to design, build, and maintain websites from conception to production. Ability to work in a fast-paced and diverse team environment developing solutions and exceeding expectations.</Col>
     </Row>
     </Container>
   );
